97939ab2255d27ccde8dfb930ddd6786952705fd
Andrew Zhang <alz2@illinois.edu>
2019-03-25 20:45:36 -0500

    Refactored code into a createMap function, wrote logic for scaling colors based on number.

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
-var map = document.createElement("div");
-map.id = "basic_choropleth"
-//style="position: relative; width: 500px; height: 300px;"
-map.style.position = "relative"
-map.style.width = "500px"
-map.style.height = "300px"
-document.body.appendChild(map);
+createMap = function(targetDiv, mapId, geoData) {
+    var map = targetDiv;
+    map.id = mapId
+    //style="position: relative; width: 500px; height: 300px;"
+    map.style.position = "relative"
+    map.style.width = "500px"
+    map.style.height = "300px"
 
 
-var basic_choropleth = new Datamap({
-    element: document.getElementById("basic_choropleth"),
-    projection: 'mercator',
-    fills: {
-        defaultFill: "#ABDDA4",
-        authorHasTraveledTo: "#fa0fa0"
-    },
-    data: {
-        USA: { fillKey: "authorHasTraveledTo" },
-        JPN: { fillKey: "authorHasTraveledTo" },
-        ITA: { fillKey: "authorHasTraveledTo" },
-        CRI: { fillKey: "authorHasTraveledTo" },
-        KOR: { fillKey: "authorHasTraveledTo" },
-        DEU: { fillKey: "authorHasTraveledTo" },
-    }
-});
-
-var colors = d3.scale.category10();
-
-window.setInterval(function() {
-    basic_choropleth.updateChoropleth({
-        USA: colors(Math.random() * 10),
-        RUS: colors(Math.random() * 100),
-        AUS: { fillKey: 'authorHasTraveledTo' },
-        BRA: colors(Math.random() * 50),
-        CAN: colors(Math.random() * 50),
-        ZAF: colors(Math.random() * 50),
-        IND: colors(Math.random() * 50),
+    var basic_choropleth = new Datamap({
+        element: document.getElementById(mapId),
+        projection: 'mercator',
+        fills: {
+            defaultFill: "#ABDDA4",
+        },
+        data: geoData
     });
-}, 2000);
+}
 
+augmentColors = function(geoData) {
+    var values = Object.keys(geoData).map((country, i) => {return geoData[country]});
+    var minValue = Math.min.apply(null, values),
+        maxValue = Math.max.apply(null, values);
+
+    var paletteScale = d3.scale.linear()
+            .domain([minValue,maxValue])
+            .range(["#EFEFFF","#02386F"]); // blue color
+
+    Object.keys(geoData).map((country, i) => {
+        geoData[country] = {
+            fillColor: paletteScale(geoData[country]),
+            number: geoData[country]
+        }
+    });
+}
+
+var target = document.createElement("div");
+var testData = {
+    USA: 100,
+    JPN: 75,
+    ITA: 50,
+    KOR: 120,
+    RUS: 10
+}
+//var testData = {
+//    USA: { fillKey: "authorHasTraveledTo" },
+//    JPN: { fillKey: "authorHasTraveledTo" },
+//    ITA: { fillKey: "authorHasTraveledTo" },
+//    CRI: { fillKey: "authorHasTraveledTo" },
+//    KOR: { fillKey: "authorHasTraveledTo" },
+//    DEU: { fillKey: "authorHasTraveledTo" },
+//}
+augmentColors(testData);
+document.body.appendChild(target);
+createMap(target, "test", testData);
